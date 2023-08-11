8ea9c8f74fbb744da4154537e5c25fdb15aa478f
Noah Pilarski <megapiggy@outerwildsmods.com>
2023-08-11 00:01:20 -0400

    Don't restock relics, quests, and research parts.

--- HarvestPOIPatch.cs
+++ HarvestPOIPatch.cs
@@ -1,4 +1,6 @@
 ﻿using HarmonyLib;
+using System.Collections.Generic;
+using Winch.Core;
 
 namespace RareFishRestock
 {
@@ -6,11 +8,29 @@ namespace RareFishRestock
     [HarmonyPatch(typeof(HarvestPOI), nameof(HarvestPOI.Start))]
     public static class HarvestPOIPatch
     {
+        public static bool HasRelic(this List<HarvestableItemData> itemList)
+        {
+            foreach (var item in itemList)
+            {
+                if (item is RelicItemData) return true;
+            }
+            return false;
+        }
+
+        public static bool IsRelicPOI(this HarvestPOIDataModel poiData)
+        {
+            return poiData.items.HasRelic() || poiData.nightItems.HasRelic();
+        }
+
         public static void Prefix(HarvestPOI __instance)
         {
-            if (__instance.harvestPOIData != null)
+            if (__instance.harvestPOIData != null && !__instance.harvestPOIData.doesRestock)
             {
-                __instance.harvestPOIData.doesRestock = true;
+                string name = __instance.gameObject.name;
+                if (!name.Contains("relic") && !name.Contains("quest-") && !name.Contains("research-item") && !__instance.harvestPOIData.IsRelicPOI())
+                {
+                    __instance.harvestPOIData.doesRestock = true;
+                }
             }
         }
     }
--- README.md
+++ README.md
@@ -3,5 +3,16 @@ Makes the fish from the Recording Rarities pursuit replenish themselves just lik
 
 The fish from that pursuit are the Oarfish, Gulper Eel, Goliath Tigerfish, and Coelacanth.
 
+There are a few other places that don't restock, but the Recording Rarities fish are the only one-time fish because they don't have another restocking fishing spot anywhere else
+
+There are a ton of unrestocking spots for the Red Snapper.
+
+The only other spots are a Ghost Shark and Barreleye (each only having one unrestockable spot)
+
+- Barreleye spot is in between twisted strand and stellar basin (E7).
+- Ghost Shark spot is by Old Fortress (B6).
+
+Another thing this mod does is restock a bunch of dredge spots.
+
 [![GitHub issues](https://img.shields.io/github/issues/MegaPiggy/DredgeRareFishRestock?style=for-the-badge)](https://github.com/MegaPiggy/DredgeRareFishRestock/issues)
 [![GitHub all releases](https://img.shields.io/github/downloads/MegaPiggy/DredgeRareFishRestock/total?style=for-the-badge)](https://github.com/MegaPiggy/DredgeRareFishRestock/releases)
